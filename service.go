8a3625aed04e4b966a2ac7f317bc7948b6a34720
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-09-07 01:41:24 +0100

    Added the microservice with JSON support

---
+++ service.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+// PriceFetcher is an interface to fetch the price.
+type PriceFetcher interface {
+	FetchPrice(context.Context, string) (float64, error)
+}
+
+// priceFetcher implements the PriceFetcher interface.
+type priceFetcher struct {}
+
+func (s *priceFetcher) FetchPrice(ctx context.Context, ticker string) (float64, error) {
+	return MockPriceFetcher(ctx, ticker)
+}
+
+var priceMocks = map[string]float64 {
+	"BTC": 20_000.00,
+	"ETH": 200.0,
+	
+}
+
+func MockPriceFetcher(ctx context.Context, ticker string) (float64, error) {
+	price, ok := priceMocks[ticker]
+	if !ok {
+		return price, fmt.Errorf("the given ticker (%s) is not supported", ticker)
+	}
+	return price, nil
+}
